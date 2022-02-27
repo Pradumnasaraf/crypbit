4752882dc79ad968e31b68dea074b90daac6c496
Atharv-777 <atharvbidwe11@gmail.com>
2022-02-27 10:29:24 +0530

    feat: Added embed reponse for bot

--- crypbit-bot/main.py
+++ crypbit-bot/main.py
@@ -1,5 +1,6 @@
 from json import load
 import os
+from turtle import color, title
 from discord.ext import commands
 import discord
 from dotenv import load_dotenv
@@ -9,6 +10,7 @@ load_dotenv()
 TOKEN = os.environ.get('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix="?")
+
 
 @bot.event
 async def on_ready():
@@ -25,13 +27,14 @@ async def hello(ctx):
 @bot.command()
 async def price(ctx, tikcerName):
     respondPrice = response.getTicketPrice(tikcerName)
-    await ctx.send(respondPrice)
+    embed = discord.Embed(title="Current value of {}".format(tikcerName), description="${}".format(respondPrice), color=0x66acba)
+    await ctx.send(embed=embed)
 
 @bot.command()
 async def whatis(ctx, tickerName):
     coinDesc = info.getCoinDesc(tickerName)
-    await ctx.send(coinDesc)
-
+    embed = discord.Embed(title=coinDesc[1], description=coinDesc[0], color=0xFFFFFF).set_thumbnail(url=coinDesc[2])
+    await ctx.send(embed=embed)
 try:
     bot.run(TOKEN)
 except discord.errors.LoginFailure as e:
